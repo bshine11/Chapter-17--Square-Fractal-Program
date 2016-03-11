783db5c942232d8da3ee14f74f5365b25ee9b60a
unknown <4068927@HS222-18.vtsd.com>
2016-03-11 08:57:28 -0500

    Second Comment

--- src/Lab17bvst.java
+++ src/Lab17bvst.java
@@ -20,10 +20,10 @@ public class Lab17bvst extends Applet
 		int midY = maxY/2;
 		int startWidth = maxX/4;
 		int startHeight = maxY/4;
-		int tlX = midX - (startWidth/2);
-		int tlY = midY - (startHeight/2);
+		int tlX = midX - (startWidth/2); // top left x
+		int tlY = midY - (startHeight/2); //top left y
 		g.fillRect(tlX,tlY,startWidth,startHeight);
-		g.fillRect(640,288,128,-96);
+
 		if(maxY <= 1){
 			g.fillRect(tlX,tlY,startWidth,startHeight);
 			
@@ -33,7 +33,11 @@ public class Lab17bvst extends Applet
 			maxX = 3*maxX/5;
 			maxY = 3*maxY/5;
 			drawSquare1(g,maxX,maxY);
-			drawSquare1(g,maxX,maxY);
+			/*g.fillRect(maxX, maxY, startHeight, startWidth);
+			g.fillRect(maxX-startHeight/2, maxY-startWidth/2,startHeight/2,startWidth/2);
+			g.fillRect(maxX+startHeight, maxY-startWidth/2,startHeight/2,startWidth/2);
+			g.fillRect(maxX-startHeight/2, maxY+startWidth,startHeight/2,startWidth/2);
+			g.fillRect(maxX+startHeight, maxY+startWidth,startHeight/2,startWidth/2);*/
 		}
 		
 	}
@@ -45,6 +49,22 @@ public class Lab17bvst extends Applet
 		long endDelay = 0;
 		while (endDelay - startDelay < n)
 			endDelay = System.nanoTime();
+	}
+
+	public void drawFourSquare(Graphics g, int x, int y, int width, int height) {
+		
+	}
+
+	public void drawSoutheast(Graphics g, int x, int y, int startWidth, int startHeight) {
+	
+	}
+
+	public void drawSouthwest(Graphics g, int x, int y, int width, int height) {
+
+	}
+
+	public void drawNorthEast(Graphics g, int x, int y, int width, int height) {
+
 	}
 
 }
